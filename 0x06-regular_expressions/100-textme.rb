b5580f474b5e88dd341f75afd9aca5d24e9f3790
AbiollaghJames <abiollajames@protonmail.com>
2023-05-03 14:00:17 -0700

    task 8

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(\+?\d+)\] \[to:(\+?\d+)\] \[flags:(-?\w:-?\w:-?\w:-?\w:-?\w)\]/).join(",")
+puts ARGV[0].scan(/\[sender:(\+?\d+)\] \[receiver:(\+?\d+)\] \[flags:(-?\w:-?\w:-?\w:-?\w:-?\w)\]/).join(",")
