e921c8f5a305f152222909589a0e2a5cf3bdf538
AbiollaghJames <abiollajames@protonmail.com>
2023-06-23 11:16:20 -0700

    Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,11 @@
-# Creating a file
-file {'/tmp/school':
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love puppet',
+#creating a file in /tmp directory
+
+file {'school':
+ensure  =>  present,
+path    =>  '/tmp/school',
+mode    =>  '0744',
+owner   =>  'www-data',
+group   =>  'www-data',
+content =>  'I love Puppet',
+
 }
