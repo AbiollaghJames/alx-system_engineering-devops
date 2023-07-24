2bb4fccceb52e9c7944d3055fe5743f54346a62b
AbiollaghJames <abiollajames@proonmail.com>
2023-07-24 22:39:56 +0300

    Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -22,4 +22,5 @@ if __name__ == "__main__":
     with open('{}.csv'.format(user_id), 'w') as csv_file:
         for task in tasks:
             csv_file.write('"{}","{}","{}","{}"\n'
-                    .format(user_id, username, task.get('completed'), task.get('title')))
+                    .format(user_id, username, task.get('completed'),
+                        task.get('title')))
