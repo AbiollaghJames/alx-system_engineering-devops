3867507311223223654239f90ecbc29d754d6e93
AbiollaghJames <abiollajames@proonmail.com>
2023-07-24 23:25:25 +0300

    Dictionary of list of dictionaries

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -22,10 +22,8 @@ if __name__ == "__main__":
         dic[user_id] = []
 
         for task in tasks:
-            dic[user_id].append({
-                "task": task.get('title'),
-                "completed": task.get('completed'),
-                "username": username
+            dic[user_id].append({"task": task.get('title'),
+                "completed": task.get('completed'), "username": username
             })
 
     with open('todo_all_employees.json', 'w') as file:
