f8a62355c63703aa8770fa74fc9fbfefd3346357
AbiollaghJames <abiollajames@proonmail.com>
2023-08-08 20:37:55 +0300

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-""" Get total number of subs """
-
-
-import requests
+""" Script to get total number of subs """
 
 
 def number_of_subscribers(subreddit):
     """ Return subs of given subreddit """
 
+    import requests
+
     baseurl = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"user-agent": "Fake-Agent"}
     response = requests.get(baseurl, headers=headers, allow_redirects=False)
-
     if response.status_code == 200:
         subs = response.json().get('data').get('subscribers')
     else:
